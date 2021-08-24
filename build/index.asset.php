ab70555c85bf7a1d99ed0e46fb67a105d8e81823
leemon <tacrapo@gmail.com>
2021-08-24 15:05:31 +0200

    Rebuild after npm deps update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '985169e0ecca8ac8dbd4556fb92ceb24');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '46ceb3dcec83ddc3e0fa67a84b070604');
