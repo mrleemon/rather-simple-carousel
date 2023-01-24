71e55623c49bf984cade6bd2db8b671f5da08399
leemon <tacrapo@gmail.com>
2023-01-24 08:05:45 +0100

    Add self-executing function

--- assets/js/carousel-gallery.js
+++ assets/js/carousel-gallery.js
@@ -1,107 +1,113 @@
-jQuery( function( $ ) {
-    
-    // carousel gallery file uploads.
-    var carousel_gallery_frame;
-    var $image_gallery_ids = $( '#carousel_items' );
-    var $carousel_images = $( '#carousel_images_container' ).find( 'ul.carousel_images' );
+(function ($) {
 
-    $( '.add_carousel_images' ).on( 'click', 'a', function( event ) {
-        var $el = $( this );
+	$(function () {
 
-        event.preventDefault();
+		// carousel gallery file uploads.
+		var carousel_gallery_frame;
+		var $image_gallery_ids = $('#carousel_items');
+		var $carousel_images = $('#carousel_images_container').find('ul.carousel_images');
 
-        // If the media frame already exists, reopen it.
-        if ( carousel_gallery_frame ) {
-            carousel_gallery_frame.open();
-            return;
-        }
+		$('.add_carousel_images').on('click', 'a', function (event) {
+			var $el = $(this);
 
-        // Create the media frame.
-        carousel_gallery_frame = wp.media.frames.carousel_gallery = wp.media({
-            // Set the title of the modal.
-            title: $el.data( 'choose' ),
-            button: {
-                text: $el.data( 'update' )
-            },
-            states: [
-                new wp.media.controller.Library({
-                    title: $el.data( 'choose' ),
-                    filterable: 'all',
-                    multiple: true
-                })
-            ]
-        });
+			event.preventDefault();
 
-        // When an image is selected, run a callback.
-        carousel_gallery_frame.on( 'select', function() {
-            var selection = carousel_gallery_frame.state().get( 'selection' );
-            var attachment_ids = $image_gallery_ids.val();
+			// If the media frame already exists, reopen it.
+			if (carousel_gallery_frame) {
+				carousel_gallery_frame.open();
+				return;
+			}
 
-            selection.map( function( attachment ) {
-                attachment = attachment.toJSON();
-                console.log(attachment);
-                if ( attachment.id ) {
-                    if ( attachment.type == 'image' ) {
-                        attachment_ids = attachment_ids ? attachment_ids + ',' + attachment.id : attachment.id;
-                        var attachment_image = attachment.sizes && attachment.sizes.thumbnail ? attachment.sizes.thumbnail.url : attachment.url;
-                        $carousel_images.append( '<li class="image" data-attachment_id="' + attachment.id + '"><img src="' + attachment_image + '" /><ul class="actions"><li><a href="#" class="delete" title="' + $el.data('delete') + '">' + $el.data('text') + '</a></li></ul></li>' );
-                    }
+			// Create the media frame.
+			carousel_gallery_frame = wp.media.frames.carousel_gallery = wp.media({
+				// Set the title of the modal.
+				title: $el.data('choose'),
+				button: {
+					text: $el.data('update')
+				},
+				states: [
+					new wp.media.controller.Library({
+						title: $el.data('choose'),
+						filterable: 'all',
+						multiple: true
+					})
+				]
+			});
 
-                }
-            });
+			// When an image is selected, run a callback.
+			carousel_gallery_frame.on('select', function () {
+				var selection = carousel_gallery_frame.state().get('selection');
+				var attachment_ids = $image_gallery_ids.val();
 
-            $image_gallery_ids.val( attachment_ids );
-        });
+				selection.map(function (attachment) {
+					attachment = attachment.toJSON();
+					console.log(attachment);
+					if (attachment.id) {
+						if (attachment.type == 'image') {
+							attachment_ids = attachment_ids ? attachment_ids + ',' + attachment.id : attachment.id;
+							var attachment_image = attachment.sizes && attachment.sizes.thumbnail ? attachment.sizes.thumbnail.url : attachment.url;
+							$carousel_images.append('<li class="image" data-attachment_id="' + attachment.id + '"><img src="' + attachment_image + '" /><ul class="actions"><li><a href="#" class="delete" title="' + $el.data('delete') + '">' + $el.data('text') + '</a></li></ul></li>');
+						}
 
-        // Finally, open the modal.
-        carousel_gallery_frame.open();
-    });
+					}
+				});
 
-    // Image ordering.
-    $carousel_images.sortable({
-        items: 'li.image',
-        cursor: 'move',
-        scrollSensitivity: 40,
-        forcePlaceholderSize: true,
-        forceHelperSize: false,
-        helper: 'clone',
-        opacity: 0.65,
-        placeholder: 'rsc-metabox-sortable-placeholder',
-        start: function( event, ui ) {
-            ui.item.css( 'background-color', '#f6f6f6' );
-        },
-        stop: function( event, ui ) {
-            ui.item.removeAttr( 'style' );
-        },
-        update: function() {
-            var attachment_ids = '';
+				$image_gallery_ids.val(attachment_ids);
+			});
 
-            $( '#carousel_images_container' ).find( 'ul li.image' ).css( 'cursor', 'default' ).each( function() {
-                var attachment_id = $( this ).attr( 'data-attachment_id' );
-                attachment_ids = attachment_ids + attachment_id + ',';
-            });
+			// Finally, open the modal.
+			carousel_gallery_frame.open();
+		});
 
-            $image_gallery_ids.val( attachment_ids );
-        }
-    });
+		// Image ordering.
+		$carousel_images.sortable({
+			items: 'li.image',
+			cursor: 'move',
+			scrollSensitivity: 40,
+			forcePlaceholderSize: true,
+			forceHelperSize: false,
+			helper: 'clone',
+			opacity: 0.65,
+			placeholder: 'rsc-metabox-sortable-placeholder',
+			start: function (event, ui) {
+				ui.item.css('background-color', '#f6f6f6');
+			},
+			stop: function (event, ui) {
+				ui.item.removeAttr('style');
+			},
+			update: function () {
+				var attachment_ids = '';
 
-    // Remove images.
-    $( '#carousel_images_container' ).on( 'click', 'a.delete', function() {
-        $( this ).closest( 'li.image' ).remove();
+				$('#carousel_images_container').find('ul li.image').css('cursor', 'default').each(function () {
+					var attachment_id = $(this).attr('data-attachment_id');
+					attachment_ids = attachment_ids + attachment_id + ',';
+				});
 
-        var attachment_ids = '';
+				$image_gallery_ids.val(attachment_ids);
+			}
+		});
 
-        $( '#carousel_images_container' ).find( 'ul li.image' ).css( 'cursor', 'default' ).each( function() {
-            var attachment_id = $( this ).attr( 'data-attachment_id' );
-            attachment_ids = attachment_ids + attachment_id + ',';
-        });
+		// Remove images.
+		$('#carousel_images_container').on('click', 'a.delete', function () {
+			$(this).closest('li.image').remove();
 
-        $image_gallery_ids.val( attachment_ids );
+			var attachment_ids = '';
 
-        // Remove any lingering tooltips.
-        $( '#tiptip_holder' ).removeAttr( 'style' );
-        $( '#tiptip_arrow' ).removeAttr( 'style' );
+			$('#carousel_images_container').find('ul li.image').css('cursor', 'default').each(function () {
+				var attachment_id = $(this).attr('data-attachment_id');
+				attachment_ids = attachment_ids + attachment_id + ',';
+			});
 
-        return false;
-    });
-});
+			$image_gallery_ids.val(attachment_ids);
+
+			// Remove any lingering tooltips.
+			$('#tiptip_holder').removeAttr('style');
+			$('#tiptip_arrow').removeAttr('style');
+
+			return false;
+		});
+
+	});
+
+})(jQuery);
+
